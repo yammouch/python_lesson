67e9fd786f315cd97ab9767e954de41c40887ebd
U-LAPTOP-ER85SC8R\tyama <tyama@preferred.jp>
2018-10-18 19:50:01 +0900

    Updated.

--- 0010_fem1d/ex01.py
+++ 0010_fem1d/ex01.py
@@ -1,7 +1,9 @@
 import numpy as np
 import numpy.linalg as LA
+import matplotlib.pyplot as plt
+import sys
 
-pos = np.linspace(0, 1, 5)
+pos = np.linspace(0, 1, 20)
 lns = pos[1:] - pos[:-1]
 
 n, = pos.shape
@@ -18,9 +20,11 @@ for i in range(n-1):
   m[i+1, i  ] += lns[i]/6.0
   m[i+1, i+1] += lns[i]/3.0
 
-#print(d)
-#print(m)
 e, v = LA.eig(np.dot(LA.inv(m), d))
 print(e)
-print(v)
+plt.plot(pos,v[:,19])
+plt.plot(pos,v[:,17])
+plt.plot(pos,v[:,16])
+plt.show()
 
+#l = sys.stdin.readline()
