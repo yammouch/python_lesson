dc30d3f92a511b0b53b01fc6a47850fc3db3ebd3
U-LAPTOP-ER85SC8R\tyama <tyama@preferred.jp>
2018-11-24 10:42:18 +0900

    Updated.

--- 0030_estate/ex01.py
+++ 0030_estate/ex01.py
@@ -3,21 +3,33 @@ import matplotlib.pyplot as plt
 def main():
   atama = 500e4
   permonth = 10e4
-  price = 3000e4
+  value_init = 3000e4
+  value_min = 500e4
   interest = 2.5 # unit : %
+  pay_year = 200e4
 
-  price_curve = []
-  price_curve.append([0, price])
+  price   = [ [0, value_init         ] ]
+  balance = [ [0, value_init - atama ] ]
 
   for i in range(1, 30):
-    price_curve.append(
-     [i, 500e4 if price_curve[-1][1] <= 700e4 else price_curve[-1][1] - 200e4])
+    price.append(
+     [ i
+     , value_min if price[-1][1] <= value_min + pay_year else
+       price[-1][1] - pay_year ] )
+    balance.append(
+     [ i
+     , 0 if balance[-1][1] <= pay_year else
+       balance[-1][1] - pay_year ] )
+    balance.append(
+     [ i
+     , 0 if balance[-1][1] == 0 else
+       balance[-1][1] * (1 + 0.01*interest) ] )
 
+  print(price)
+  print([x[0] for x in price])
 
-  print(price_curve)
-  print([x[0] for x in price_curve])
-
-  plt.plot([x[0] for x in price_curve], [x[1] for x in price_curve])
+  plt.plot([x[0] for x in price  ], [x[1] for x in price  ])
+  plt.plot([x[0] for x in balance], [x[1] for x in balance])
   plt.show()
 
   #paid = []
