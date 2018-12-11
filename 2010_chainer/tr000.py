b6bb5bf59f1d25f0001e97b6065daa74bd68d98b
U-LAPTOP-ER85SC8R\tyama <tyama@preferred.jp>
2018-12-11 09:16:06 +0900

    Updated.

--- 2010_chainer/tr000.py
+++ 2010_chainer/tr000.py
@@ -27,36 +27,42 @@ class MLP(chainer.Chain):
 
 def main():
   parser = argparse.ArgumentParser(description='Chainer example: MNIST')
-  parser.add_argument('--batchsize', '-b', type=int, default=100,
-                      help='Number of images in each mini-batch')
-  parser.add_argument('--epoch', '-e', type=int, default=20,
-                      help='Number of sweeps over the dataset to train')
-  parser.add_argument('--frequency', '-f', type=int, default=-1,
-                      help='Frequency of taking a snapshot')
-  parser.add_argument('--gpu', '-g', type=int, default=-1,
-                      help='GPU ID (negative value indicates CPU)')
-  parser.add_argument('--out', '-o', default='result',
-                      help='Directory to output the result')
-  parser.add_argument('--resume', '-r', default='',
-                      help='Resume the training from snapshot')
-  parser.add_argument('--unit', '-u', type=int, default=1000,
-                      help='Number of units')
-  parser.add_argument('--noplot', dest='plot', action='store_false',
-                      help='Disable PlotReport extension')
-  args = parser.parse_args()
-  args.epoch =   1
-  args.unit  = 100
+  #parser.add_argument('--batchsize', '-b', type=int, default=100,
+  #                    help='Number of images in each mini-batch')
+  #parser.add_argument('--epoch', '-e', type=int, default=20,
+  #                    help='Number of sweeps over the dataset to train')
+  #parser.add_argument('--frequency', '-f', type=int, default=-1,
+  #                    help='Frequency of taking a snapshot')
+  #parser.add_argument('--gpu', '-g', type=int, default=-1,
+  #                    help='GPU ID (negative value indicates CPU)')
+  #parser.add_argument('--out', '-o', default='result',
+  #                    help='Directory to output the result')
+  #parser.add_argument('--resume', '-r', default='',
+  #                    help='Resume the training from snapshot')
+  #parser.add_argument('--unit', '-u', type=int, default=1000,
+  #                    help='Number of units')
+  #parser.add_argument('--noplot', dest='plot', action='store_false',
+  #                    help='Disable PlotReport extension')
+  #args = parser.parse_args()
+  #args.epoch =   1
+  #args.unit  = 100
+  #args.batchsize = 100
+  #epoch     =   1
+  #unit      = 100
+  batchsize = 100
 
   #print('GPU: {}'.format(args.gpu))
-  print('# unit: {}'.format(args.unit))
-  print('# Minibatch-size: {}'.format(args.batchsize))
-  print('# epoch: {}'.format(args.epoch))
-  print('')
+  #print('# unit: {}'.format(args.unit))
+  #print('# Minibatch-size: {}'.format(args.batchsize))
+  #print('# epoch: {}'.format(args.epoch))
+  #print('')
 
   # Set up a neural network to train
   # Classifier reports softmax cross entropy loss and accuracy at every
   # iteration, which will be used by the PrintReport extension below.
-  model = L.Classifier(MLP(args.unit, 10))
+  #model = L.Classifier(MLP(args.unit, 10))
+  #model = L.Classifier(MLP(unit, 10))
+  model = L.Classifier(MLP(100, 10))
   #if args.gpu >= 0:
   #  # Make a specified GPU current
   #  chainer.backends.cuda.get_device_from_id(args.gpu).use()
@@ -69,8 +75,11 @@ def main():
   # Load the MNIST dataset
   train, test = chainer.datasets.get_mnist()
 
-  train_iter = chainer.iterators.SerialIterator(train, args.batchsize)
-  test_iter = chainer.iterators.SerialIterator(test, args.batchsize,
+  #train_iter = chainer.iterators.SerialIterator(train, args.batchsize)
+  #test_iter = chainer.iterators.SerialIterator(test, args.batchsize,
+  #                                             repeat=False, shuffle=False)
+  train_iter = chainer.iterators.SerialIterator(train, batchsize)
+  test_iter = chainer.iterators.SerialIterator(test, batchsize,
                                                repeat=False, shuffle=False)
 
   # Set up a trainer
@@ -78,7 +87,10 @@ def main():
   #  train_iter, optimizer, device=args.gpu)
   updater = training.updaters.StandardUpdater(
     train_iter, optimizer, device=-1)
-  trainer = training.Trainer(updater, (args.epoch, 'epoch'), out=args.out)
+  #trainer = training.Trainer(updater, (args.epoch, 'epoch'), out=args.out)
+  #trainer = training.Trainer(updater, (args.epoch, 'epoch'), out='result')
+  #trainer = training.Trainer(updater, (epoch, 'epoch'), out='result')
+  trainer = training.Trainer(updater, (1, 'epoch'), out='result')
 
   # Evaluate the model with the test dataset for each epoch
   #trainer.extend(extensions.Evaluator(test_iter, model, device=args.gpu))
@@ -89,21 +101,22 @@ def main():
   trainer.extend(extensions.dump_graph('main/loss'))
 
   # Take a snapshot for each specified epoch
-  frequency = args.epoch if args.frequency == -1 else max(1, args.frequency)
-  trainer.extend(extensions.snapshot(), trigger=(frequency, 'epoch'))
+  #frequency = args.epoch if args.frequency == -1 else max(1, args.frequency)
+  #trainer.extend(extensions.snapshot(), trigger=(frequency, 'epoch'))
+  trainer.extend(extensions.snapshot(), trigger=(1, 'epoch'))
 
   # Write a log of evaluation statistics for each epoch
   trainer.extend(extensions.LogReport())
 
   # Save two plot images to the result dir
-  if args.plot and extensions.PlotReport.available():
-    trainer.extend(
-      extensions.PlotReport(['main/loss', 'validation/main/loss'],
-                            'epoch', file_name='loss.png'))
-    trainer.extend(
-      extensions.PlotReport(
-       ['main/accuracy', 'validation/main/accuracy'],
-       'epoch', file_name='accuracy.png'))
+  #if args.plot and extensions.PlotReport.available():
+  #  trainer.extend(
+  #    extensions.PlotReport(['main/loss', 'validation/main/loss'],
+  #                          'epoch', file_name='loss.png'))
+  #  trainer.extend(
+  #    extensions.PlotReport(
+  #     ['main/accuracy', 'validation/main/accuracy'],
+  #     'epoch', file_name='accuracy.png'))
 
   # Print selected entries of the log to stdout
   # Here "main" refers to the target link of the "main" optimizer again, and
@@ -117,9 +130,9 @@ def main():
   # Print a progress bar to stdout
   trainer.extend(extensions.ProgressBar())
 
-  if args.resume:
-    # Resume from a snapshot
-    chainer.serializers.load_npz(args.resume, trainer)
+  #if args.resume:
+  #  # Resume from a snapshot
+  #  chainer.serializers.load_npz(args.resume, trainer)
 
   # Run the training
   trainer.run()
