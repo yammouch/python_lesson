afd86136e04c9e29686b6ba071f65e48d5428e64
U-LAPTOP-ER85SC8R\tyama <tyama@preferred.jp>
2018-12-30 14:56:00 +0900

    First checkin.

--- 2030_np_loadtxt/pr020.py
+++ 2030_np_loadtxt/pr020.py
@@ -1,1 +1,10 @@
-from chainer.links.connection import convolution_2d
+import numpy as np
+import chainer.links.connection.convolution_2d as C
+
+np.random.seed(1)
+
+init_w = np.zeros([1, 1, 3, 2], dtype=np.float32)
+
+l1 = C.Convolution2D(3, 2, initialW=init_w)
+print(l1.W)
+#print(l1.W.shape)
