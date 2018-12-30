7642a26a9f04bf79238bcc76fb9660724542d2dc
U-LAPTOP-ER85SC8R\tyama <tyama@preferred.jp>
2018-12-30 17:46:37 +0900

    Now it parse my schematic field.

--- 2030_np_loadtxt/pr040.py
+++ 2030_np_loadtxt/pr040.py
@@ -1,5 +1,25 @@
 import numpy as np
 
+def format_field(ar):
+  acc = np.zeros(ar.shape[1:])
+  for i in range(ar.shape[0]):
+    acc += ar[i] * 2**i
+  sacc = []
+  for i in range(ar.shape[1]):
+    line = ','.join(['  ' if x == 0 else '{:02X}'.format(int(x))
+                    for x in acc[i]])
+    sacc.append(line)
+  return sacc
+
 a = np.loadtxt("../../jocl_lesson/0270_sasimi/train_in.dat", dtype=np.float32)
 print(a.shape)
-print(np.moveaxis(a.reshape(-1, 10, 10, 6), 3, 1).shape)
+b = np.moveaxis(a.reshape(-1, 10, 10, 6), 3, 1)
+print(b.shape)
+
+#print(a[0, ..., ..., ...].shape)
+#print(a[0, :, :, :].shape)
+print(type(b[0]))
+print((b[0]).shape)
+print((b[0]).shape[0])
+for line in format_field(b[0]):
+  print(line)
