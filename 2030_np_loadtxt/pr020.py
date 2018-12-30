2b65a51bf36592b606e1a7c649e1139b4f03b798
U-LAPTOP-ER85SC8R\tyama <tyama@preferred.jp>
2018-12-30 15:37:43 +0900

    Updated.

--- 2030_np_loadtxt/pr020.py
+++ 2030_np_loadtxt/pr020.py
@@ -1,10 +1,14 @@
 import numpy as np
-import chainer.links.connection.convolution_2d as C
+import chainer.links as L
 
 np.random.seed(1)
 
-init_w = np.zeros([1, 1, 3, 2], dtype=np.float32)
+init_w = np.zeros([2, 3, 1, 1], dtype=np.float32)
 
-l1 = C.Convolution2D(3, 2, initialW=init_w)
+#l1 = L.Convolution2D(3, 2, 1)
+l1 = L.Convolution2D(3, 2, 1, initialW=init_w)
+i1 = np.arange(1 * 3 * 5 * 4, dtype=np.float32).reshape(1, 3, 5, 4)
+print(i1)
+print(l1(i1))
 print(l1.W)
-#print(l1.W.shape)
+print(l1.W.shape)
