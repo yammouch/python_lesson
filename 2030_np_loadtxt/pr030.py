8b2f1795fcaee6c2fc8d061c18ab2b14f4f309b6
U-LAPTOP-ER85SC8R\tyama <tyama@preferred.jp>
2018-12-30 16:05:58 +0900

    First checkin.

---
+++ 2030_np_loadtxt/pr030.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+a = np.arange(5 * 4 * 3 * 2, dtype=np.int32)
+acc = np.zeros(5 * 4 * 3 * 2, dtype=np.int32)
+
+r = a % 2
+acc += r
+a = a // 2
+
+r = a % 3
+acc += r*10
+a = a // 3
+
+r = a % 4
+acc += r*100
+a = a // 4
+
+r = a % 5
+acc += r*1000
+a = a // 5
+
+print(acc)
