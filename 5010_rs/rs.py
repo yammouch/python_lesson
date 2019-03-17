59f277ab19be70e3416ba8bb9d88bde1175d3a08
U-LAPTOP-ER85SC8R\tyama <tyama@preferred.jp>
2019-03-18 08:42:44 +0900

    Updated.

--- 5010_rs/rs.py
+++ 5010_rs/rs.py
@@ -1,16 +1,17 @@
-p2b = [0] * 0xFF
+p2b = [0] * 0x100
+p2b[-1] = 0
 p2b[0] = 0x01
-for i in range(0xFF):
+for i in range(0xFE):
   shifted = (p2b[i] << 1) & 0xFF
-  p2b[i+1] = shifted ^ 0x71 if p2b[i] (1 << 7) else shifted
+  p2b[i+1] = shifted ^ 0x71 if p2b[i] & (1 << 7) else shifted
 
 b2p = [0] * 0x100
 b2p[0xFF] = 0
 for i in range(0xFF):
-  b2b[p2b[i]] = i
+  b2p[p2b[i]] = i
 
 def addc(x1, x2):
-  return b2b[p2b[x1] ^ p2b[x2]]
+  return b2p[p2b[x1] ^ p2b[x2]]
 
 def mulc(x1, x2):
   if 0xFF <= x1 or 0xFF <= x2:
@@ -28,7 +29,7 @@ def divp_inplace(n, d):
   for i in range(len(n)-len(d)+1):
     n[i] = divc(n[i], d[i])
     for j in range(1, len(d)):
-      n[i+j] = addc(n[i+j], mulc(d[i+j], n[i]))
+      n[i+j] = addc(n[i+j], mulc(d[j], n[i]))
 
 def assign(p, x):
   acc = 0xFF
@@ -38,9 +39,9 @@ def assign(p, x):
 
 # (x + 1)(x + a)
 # = x^2 + (a + 1)x + a
-def rs_encode(l):
+def encode(l):
   a = l + [0xFF] * 2
-  divp_inplace(a, [b2p(x) for x in [0x01, 0x03, 0x02]])
+  divp_inplace(a, [b2p[x] for x in [0x01, 0x03, 0x02]])
   return a
 
 def syndrome(l):
