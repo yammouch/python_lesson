d889980b923afa7e767587ca0074d677cda424ad
U-LAPTOP-ER85SC8R\tyama <tyama@preferred.jp>
2019-03-18 08:21:35 +0900

    Updated.

--- 5010_rs/rs.py
+++ 5010_rs/rs.py
@@ -9,8 +9,23 @@ b2p[0xFF] = 0
 for i in range(0xFF):
   b2b[p2b[i]] = i
 
+def addc(x1, x2):
+  return b2b[p2b[x1] ^ p2b[x2]]
+
+def mulc(x1, x2):
+  if 0xFF <= x1 or 0xFF <= x2:
+    return 0xFF
+  else:
+    return (x1 + x2) % 0xFF
+
+def divc(n, d):
+  if 0xFF <= d:
+    raise ValueError()
+  else:
+    return (n - d) % 0xFF
+
 def divp_inplace(n, d):
   for i in range(len(n)-len(d)+1):
-    mlier = divc(n[i], d[i])
-    adder = [mulc(x, mlier) for x in d]
-    for j in range(len(d)):
+    q = divc(n[i], d[i])
+    for j in range(1, len(d)):
+      n[i+j] = addc(n[i+j], mulc(d[i+j], q))
