2a3be32a71170990dcb5190d97bad1441cb5c315
Takuya Yamauchi <tyama@preferred.jp>
2019-05-10 22:06:01 +0900

    Updated.

--- 2050_schem/meander.py
+++ 2050_schem/meander.py
@@ -186,19 +186,19 @@ def ring_0(size, l):
 #
 #                    |<-l4->|<- l5 ->|
 
-#(defn ring-1-points [l]
-#  (let [y0 (+ (l 2) (l 3))
-#        x0 (- (l 4) (l 0) 2 (l 1))
-#        p0 [(if (< y0 0) (- y0) 0)
-#            (if (< 0 x0) x0 0)]
-#        p1 (update-in p0 [1] + (l 0))
-#        p2 (update-in p1 [1] + 2)
-#        p3 (update-in p2 [1] + (l 1))
-#        p4 (update-in p3 [0] + (l 2) (l 3))
-#        p5 (update-in p4 [1] - (l 4))
-#        p6 (update-in p5 [0] - (l 3))
-#        p7 (update-in p6 [1] + (l 4) (l 5))]
-#    [p0 p1 p2 p3 p4 p5 p6 p7]))
+def ring_1_points(l):
+  y0 = l[2] + l[3]
+  x0 = l[4] - l[0] - 2 - l[1]
+  p0 = [-y0 if y0 < 0 else 0,
+         x0 if 0 < x0 else 0]
+  p1 = [p0[0]              , p0[1] + l[0]       ]
+  p2 = [p1[0]              , p1[1] + 2          ]
+  p3 = [p2[0]              , p2[1] + l[1]       ]
+  p4 = [p3[0] + l[2] + l[3], p3[1]              ]
+  p5 = [p4[0]              , p4[1] - l[4]       ]
+  p6 = [p5[0] - l[3]       , p5[1]              ]
+  p7 = [p6[0]              , p6[1] + l[4] + l[5]]
+  return [p0, p1, p2, p3, p4, p5, p6, p7]
 
 #(defn ring-1-0 [[h w] l]
 #  (let [[p0 p1 p2 p3 p4 p5 p6 p7] (ring-1-points l)]
