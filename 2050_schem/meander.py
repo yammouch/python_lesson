e1324c3b58270a4b93cca78cedd7fe872fdef6a3
Takuya Yamauchi <tyama@preferred.jp>
2019-05-30 10:46:01 +0900

    Updated.

--- 2050_schem/meander.py
+++ 2050_schem/meander.py
@@ -1,4 +1,5 @@
-import copy
+from copy import deepcopy
+from functools import reduce
 import schemprep as smp
 #import schemmlp as scp
 import util as utl
@@ -26,20 +27,21 @@ def range_n(from_p, to, o):
   return range_2d(0, from_p, to, o)
 
 def line(field, from_p, to, o):
-  fld = copy.deepcopy(field)
+  fld = deepcopy(field)
   for y, x in range_n(from_p, to, o):
     fld[y][x][o] = 1
   return fld
 
 def lines(field, from_p, tos):
-  fld = copy.deepcopy(field)
+  fld = deepcopy(field)
   for p, o in tos:
     fld = line(fld, from_p, p, o)
     from_p = p
   return fld
 
 def add_elements(field, els):
-  fld = copy.deepcopy(field)
+  print(els)
+  fld = deepcopy(field)
   for p, d in els:
     fld[p[0]][p[1]][d] = 1
   return fld
@@ -73,8 +75,8 @@ def meander_0_points(l):
 
 def meander_0_0(size, l):
   y0, p0, p1, p2, p3, p4, p5, p6, p7 = meander_0_points(l)
-  fld = [ [ [0] * 6 for _ in range(size[1]) ]
-            for _ in range(size[0]) ]
+  fld = reduce(lambda acc, x: [deepcopy(acc) for _ in range(x)],
+               [6] + size[::-1], 0)
   fld = add_elements(fld, [[p0, 3], [p5, 5], [p7, 4]])
   fld = lines(fld, p0, [[p1, 1], [p2, 0], [p3, 1], [p4, 0], [p5, 1]])
   fld = line(fld, p6, p7[1], 1)
@@ -85,8 +87,8 @@ def meander_0_0(size, l):
 
 def meander_0_1(size, l):
   y0, p0, _, _, _, p4, p5, p6, p7 = meander_0_points(l)
-  fld = [ [ [0] * 6 for _ in range(size[1]) ]
-            for _ in range(size[0]) ]
+  fld = reduce(lambda acc, x: [deepcopy(acc) for _ in range(x)],
+               [6] + size[::-1], 0)
   fld = add_elements(fld, [[p0, 3], [p5, 5], [p7, 4]])
   fld = line(fld, p0, p4[1], 1)
   fld = lines(fld, [p0[0], p4[1]], [[p4, 0], [p5, 1]])
@@ -127,7 +129,7 @@ def ring_0_points(l):
   p0 = [-y0 if y0 < 0 else 0, -x0 if x0 < 0 else 0]
   p1 = [ p0[0]              , p0[1] + l[0]        ]
   p2 = [ p1[0] + l[1] + l[2], p1[1]               ]
-  p3 = [ p2[0]              , p2[1] - l[2]        ]
+  p3 = [ p2[0]              , p2[1] - l[3]        ]
   p4 = [ p3[0] - l[2]       , p3[1]               ]
   p5 = [ p4[0]              , p4[1] + l[3] + l[4] ]
   p6 = [ p5[0]              , p5[1] +   2         ]
@@ -136,8 +138,8 @@ def ring_0_points(l):
 
 def ring_0_0(size, l):
   p0, p1, p2, p3, p4, p5, p6, p7 = ring_0_points(l)
-  fld = [ [ [0] * 6 for _ in range(size[1]) ]
-            for _ in range(size[0]) ]
+  fld = reduce(lambda acc, x: [deepcopy(acc) for _ in range(x)],
+               [6] + size[::-1], 0)
   fld = add_elements(fld, [[p0, 3], [p5, 5], [p7, 4]])
   fld = lines(fld, p0, [[p1, 1], [p2, 0], [p3, 1], [p4, 0], [p5, 1]])
   fld = line(fld, p6, p7[1], 1)
@@ -148,8 +150,8 @@ def ring_0_0(size, l):
 
 def ring_0_1(size, l):
   p0, p1, _, _, p4, p5, p6, p7 = ring_0_points(l)
-  fld = [ [ [0] * 6 for _ in range(size[1]) ]
-            for _ in range(size[0]) ]
+  fld = reduce(lambda acc, x: [deepcopy(acc) for _ in range(x)],
+               [6] + size[::-1], 0)
   fld = add_elements(fld, [[p0, 3], [p5, 5], [p7, 4], [[p4[0], p1[1]], 2]])
   fld = lines(fld, p0, [[p1, 1], [p4, 0], [p5, 1]])
   fld = line(fld, p6, p7[1], 1)
@@ -160,8 +162,8 @@ def ring_0_1(size, l):
 
 def ring_0_2(size, l):
   p0, p1, _, _, _, p5, p6, p7 = ring_0_points(l)
-  fld = [ [ [0] * 6 for _ in range(size[1]) ]
-            for _ in range(size[0]) ]
+  fld = reduce(lambda acc, x: [deepcopy(acc) for _ in range(x)],
+               [6] + size[::-1], 0)
   fld = add_elements(fld, [[p0, 3], [p5, 5], [p7, 4]])
   fld = lines(fld, p0, [[p1, 1], [[p5[0], p1[1]], 0], [p5, 1]])
   fld = line(fld, p6, p7[1], 1)
@@ -204,8 +206,8 @@ def ring_1_points(l):
 
 def ring_1_0(size, l):
   p0, p1, p2, p3, p4, p5, p6, p7 = ring_1_points(l)
-  fld = [ [ [0] * 6 for _ in range(size[1]) ]
-            for _ in range(size[0]) ]
+  fld = reduce(lambda acc, x: [deepcopy(acc) for _ in range(x)],
+               [6] + size[::-1], 0)
   fld = add_elements(fld, [[p0, 3], [p5, 5], [p7, 4]])
   fld = line(fld, p0, p1[1], 1)
   fld = lines(fld, p2, [[p3, 1], [p4, 0], [p5, 1], [p6, 0], [p7, 1]])
@@ -216,8 +218,8 @@ def ring_1_0(size, l):
 
 def ring_1_1(size, l):
   p0, p1, p2, p3, p4, p5, p6, p7 = ring_1_points(l)
-  fld = [ [ [0] * 6 for _ in range(size[1]) ]
-            for _ in range(size[0]) ]
+  fld = reduce(lambda acc, x: [deepcopy(acc) for _ in range(x)],
+               [6] + size[::-1], 0)
   fld = add_elements(fld, [[p0, 3], [p5, 5], [p7, 4], [[p6[0], p3[1]], 2]])
   fld = line(fld, p0, p1[1], 1)
   fld = lines(fld, p2, [[p3, 1], [p6, 0], [p7, 1]])
@@ -228,8 +230,8 @@ def ring_1_1(size, l):
 
 def ring_1_2(size, l):
   p0, p1, p2, p3, p4, p5, p6, p7 = ring_1_points(l)
-  fld = [ [ [0] * 6 for _ in range(size[1]) ]
-            for _ in range(size[0]) ]
+  fld = reduce(lambda acc, x: [deepcopy(acc) for _ in range(x)],
+               [6] + size[::-1], 0)
   fld = add_elements(fld, [[p0, 3], [p5, 5], [p7, 4]])
   fld = line(fld, p0, p1[1], 1)
   fld = lines(fld, p2, [[p3, 1], [[p6[0], p3[1]], 0], [p7, 1]])
