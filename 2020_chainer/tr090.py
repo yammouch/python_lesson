7c017e93d3cc3c0ef1be8d01b86f2ae90b65f236
Takuya Yamauchi <tyama@preferred.jp>
2019-08-15 18:57:42 +0900

    Updated.

--- 2020_chainer/tr090.py
+++ 2020_chainer/tr090.py
@@ -14,13 +14,13 @@ from chainer.training import extensions
 
 np.random.seed(1)
 
-batchsize = 2
-inputs = np.array([[1.0], [-1.0]], dtype=np.float32)
-outputs = np.array([[1], [0]], dtype=np.int8)
-
-#batchsize = 1
-#inputs = np.array([[1.0]], dtype=np.float32)
+inputs = np.array([[0, 1], [0, 1]], dtype=np.float32)
 #outputs = np.array([[1]], dtype=np.int8)
+outputs = np.array([1, 1], dtype=np.int8)
+#inputs = np.array([[1, 0], [0, 1]], dtype=np.float32)
+#outputs = np.array([[1], [0]], dtype=np.int8)
+
+batchsize = inputs.shape[0]
 
 train = chainer.datasets.TupleDataset(inputs, outputs) 
 test = chainer.datasets.TupleDataset(inputs, outputs) 
@@ -28,13 +28,16 @@ test = chainer.datasets.TupleDataset(inputs, outputs)
 train_iter = iterators.SerialIterator(train, batchsize)
 test_iter = iterators.SerialIterator(test, batchsize, False, False)
 
+
 class MLP(Chain):
 
     def __init__(self, n_out=1):
         super(MLP, self).__init__()
         with self.init_scope():
             self.l1 = L.Linear(None, n_out,
-             initialW=np.array([[np.log(2)]], dtype=np.float32))
+             initialW=np.array(
+              [ [np.log(1), np.log(2)] ,
+                [        0,         0] ], dtype=np.float32))
 
     def forward(self, x):
         return self.l1(x)
@@ -42,13 +45,14 @@ class MLP(Chain):
 #gpu_id = 0  # Set to -1 if you use CPU
 gpu_id = -1  # Set to -1 if you use CPU
 
-model = MLP()
+model = MLP(n_out=2)
 if gpu_id >= 0:
     model.to_gpu(gpu_id)
 
 max_epoch = 1
 
-model = L.Classifier(model, lossfun=F.sigmoid_cross_entropy)
+#model = L.Classifier(model, lossfun=F.sigmoid_cross_entropy)
+model = L.Classifier(model, lossfun=F.softmax_cross_entropy)
 model.compute_accuracy = False
 
 optimizer = optimizers.SGD()
@@ -59,6 +63,11 @@ trainer = training.Trainer(updater, (max_epoch, 'epoch'), out='tr080_result')
 trainer.extend(extensions.LogReport())
 trainer.extend(extensions.Evaluator(test_iter, model, device=gpu_id))
 trainer.extend(extensions.PrintReport(['epoch', 'main/loss', 'validation/main/loss', 'elapsed_time']))
+
+print(model.predictor.l1.W)
+print(model.predictor.l1.W.grad)
+print(model.predictor.l1.b)
+print(model.predictor.l1.b.grad)
 
 trainer.run()
 
