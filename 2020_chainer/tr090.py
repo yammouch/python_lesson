20e05fa1cb3cb0a40c22c73549a010e78d9dd399
Takuya Yamauchi <tyama@preferred.jp>
2019-08-15 18:26:45 +0900

    Updated.

--- 2020_chainer/tr090.py
+++ 2020_chainer/tr090.py
@@ -1,16 +1,13 @@
 # - remove b
-# - remove unnecessary portion
+# - [done] remove unnecessary portion
 # - change input vector length to 2
 # - change output data for softmax, and use softmax
 
 import numpy as np
 import chainer
-from chainer import backend
-from chainer import backends
-from chainer.backends import cuda
-from chainer import Function, gradient_check, report, training, utils, Variable
-from chainer import datasets, iterators, optimizers, serializers
-from chainer import Link, Chain, ChainList
+from chainer import training
+from chainer import datasets, iterators, optimizers
+from chainer import Link, Chain
 import chainer.functions as F
 import chainer.links as L
 from chainer.training import extensions
@@ -51,34 +48,17 @@ if gpu_id >= 0:
 
 max_epoch = 1
 
-# Wrap your model by Classifier and include the process of loss calculation within your model.
-# Since we do not specify a loss function here, the default 'softmax_cross_entropy' is used.
-#model = L.Classifier(model)
 model = L.Classifier(model, lossfun=F.sigmoid_cross_entropy)
 model.compute_accuracy = False
 
-# selection of your optimizing method
-#optimizer = optimizers.MomentumSGD()
 optimizer = optimizers.SGD()
-
-# Give the optimizer a reference to the model
 optimizer.setup(model)
-
-# Get an updater that uses the Iterator and Optimizer
 updater = training.updaters.StandardUpdater(train_iter, optimizer, device=gpu_id)
-
-# Setup a Trainer
 trainer = training.Trainer(updater, (max_epoch, 'epoch'), out='tr080_result')
 
-from chainer.training import extensions
-
 trainer.extend(extensions.LogReport())
-trainer.extend(extensions.snapshot(filename='snapshot_epoch-{.updater.epoch}'))
-trainer.extend(extensions.snapshot_object(model.predictor, filename='model_epoch-{.updater.epoch}'))
 trainer.extend(extensions.Evaluator(test_iter, model, device=gpu_id))
 trainer.extend(extensions.PrintReport(['epoch', 'main/loss', 'validation/main/loss', 'elapsed_time']))
-trainer.extend(extensions.PlotReport(['main/loss', 'validation/main/loss'], x_key='epoch', file_name='loss.png'))
-trainer.extend(extensions.dump_graph('main/loss'))
 
 trainer.run()
 
@@ -86,22 +66,3 @@ print(model.predictor.l1.W)
 print(model.predictor.l1.W.grad)
 print(model.predictor.l1.b)
 print(model.predictor.l1.b.grad)
-
-import matplotlib.pyplot as plt
-
-model = MLP()
-serializers.load_npz('tr080_result/model_epoch-1', model)
-
-print('model.l1.W:', model.l1.W)
-print('model.l1.b:', model.l1.b)
-
-for i in range(2):
-  # Show the output
-  x, t = test[i]
-  print('input:', x)
-  print('label:', t)
-
-  y = model(x[None, ...])
-
-  print('output:', y.data)
-  #print('predicted_label:', y.data.argmax(axis=1)[0])
