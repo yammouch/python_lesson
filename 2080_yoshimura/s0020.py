8a3199efde94672609dba9af56c22bfaec5ae543
Takuya Yamauchi <tyama@preferred.jp>
2019-12-30 16:25:45 +0900

    First checkin.

--- 2080_yoshimura/s0020.py
+++ 2080_yoshimura/s0020.py
@@ -29,6 +29,16 @@ def dist(g):
 
   return retval
 
+def htruck_range(u, l):
+  retval = {}
+  for i, pair in enumerate(zip(u, l)):
+    for x in pair:
+      if x in retval:
+        retval[x][1] = i
+      else:
+        retval[x] = [i, None]
+  return retval
+
 def main():
   g = { 'a': ['b', 'c']
       , 'b': ['c'     ]
@@ -37,6 +47,7 @@ def main():
   # ['a']
   print(origins(g))
   print(dist(g))
+  print(htruck_range([None, 1, 2, 3], [3, 2, None, 1]))
 
 if __name__ == '__main__':
   main()
