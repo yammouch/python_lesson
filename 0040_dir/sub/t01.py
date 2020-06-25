68e1ec783176a0de3c788427f6ac86a33203406f
Takuya Yamauchi <tyama@preferred.jp>
2020-06-25 10:39:57 +0900

    First checkin.

---
+++ 0040_dir/sub/t01.py
@@ -0,0 +1,5 @@
+from __future__ import print_function
+#import sub.m01 as M
+import m01 as M
+
+print(M.inc(1))
---
+++ 0040_dir/t02.py
@@ -0,0 +1,4 @@
+from __future__ import print_function
+import a0010_schem.env_0010 as env_0010
+
+print(env_0010.inc(1))
